4c7ef71ac9ff65b68f544aa3a8a37ea045f00c00
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-04-06 11:13:35 +0200

    Remove try catch from 'getUserByEmail' method

--- api/features/user/repositories/user.repository.js
+++ api/features/user/repositories/user.repository.js
@@ -3,15 +3,8 @@ import ValidationError from "../../../errors/ValidationError.js";
 
 class UserRepository {
     static async getUserByEmail(email) {
-        try {
-            const [rows] = await dbPool.query("SELECT * FROM user WHERE email=?", [email]);
-            return rows;
-        } catch (error) {
-            throw new ValidationError({
-                type: "critical",
-                message: error.message,
-            });
-        }
+        const [rows] = await dbPool.query("SELECT * FROM user WHERE email=?", [email]);
+        return rows;
     }
 
 
